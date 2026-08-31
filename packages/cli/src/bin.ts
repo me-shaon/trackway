db15f14e437c5b6e2c53f50fc8bd531bd2c36318
Ahmed shamim <shaon.cse81@gmail.com>
2026-08-31 19:50:52 +0600

    chore: release v0.4.1

--- packages/cli/src/bin.ts
+++ packages/cli/src/bin.ts
@@ -31,7 +31,7 @@ async function main(): Promise<void> {
   program
     .name('trackway')
     .description('Answers why a line of code exists, using the agent session behind it.')
-    .version('0.4.0')
+    .version('0.4.1')
     .showHelpAfterError();
 
   program
